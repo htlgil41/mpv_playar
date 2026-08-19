4afe7730b27b1ad1a8f600514644913b100316ee
Aaron Gil <htlgil41@gmail.com>
2026-08-19 08:47:39 -0400

    comando de estado en el servidor pipe

--- internal/libs/serverUnix_linux.go
+++ internal/libs/serverUnix_linux.go
@@ -1,6 +1,9 @@
 package libs
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
 type ConnectionUnix struct {
 	Connect net.Conn
@@ -10,6 +13,11 @@ func ServerSocketForUnix(path string) (*ConnectionUnix, error) {
 	con, err_con := net.Dial("unix", path)
 	if err_con != nil {
 		return nil, err_con
+	}
+
+	_, err_writestatus := con.Write([]byte(`{ "command": ["get_property", "mpv-version"] }` + "\n"))
+	if err_writestatus != nil {
+		fmt.Println("Error al escribir el comando de estado en mpv")
 	}
 	return &ConnectionUnix{Connect: con}, nil
 }
--- internal/libs/serverUnix_windows.go
+++ internal/libs/serverUnix_windows.go
@@ -1,6 +1,7 @@
 package libs
 
 import (
+	"fmt"
 	"net"
 
 	"github.com/Microsoft/go-winio"
@@ -14,6 +15,11 @@ func ServerSocketForUnix(path string) (*ConnectionUnix, error) {
 	con, err_con := winio.DialPipe(path, nil)
 	if err_con != nil {
 		return nil, err_con
+	}
+
+	_, err_writestatus := con.Write([]byte(`{ "command": ["get_property", "mpv-version"] }` + "\n"))
+	if err_writestatus != nil {
+		fmt.Println("Error al escribir el comando de estado en mpv")
 	}
 	return &ConnectionUnix{Connect: con}, nil
 }
