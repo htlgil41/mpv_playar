504c6e8aed3d4d60decab6813e8d2238299366d0
Aaron Gil <htlgil41@gmail.com>
2026-08-20 11:07:19 -0400

    no compression

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -17,9 +17,8 @@ import (
 )
 
 var upgrader = websocket.Upgrader{
-	EnableCompression: true,
-	ReadBufferSize:    1024,
-	WriteBufferSize:   1024,
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
